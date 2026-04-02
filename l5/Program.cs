34acd9e246c22a983bd7c121f57d48754e17fd98
gzlm163 <marusyagzl@gmail.com>
2026-04-02 15:51:51 +0700

    Added a dictionary of misspelled words.

---
+++ l5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Program {
+  static void Main(string[] args) {
+    Dictionary<string, string[]> misspelledWords = new Dictionary<string, string[]> {
+      { "привет", new string[] { "првиет", "пирвет", "првиет"} },
+      { "пожалуйста", new string[] { "пожлауйста", "пожалусйта", "пожлуйста" } },
+      { "спасибо", new string[] { "спаисбо", "спассбо", "сапсибо"} },
+      { "конечно", new string[] { "коенчно", "коненчо", "конеечно"} }
+    };
+
+    
+  }
+
+  
+}
